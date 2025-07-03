bdaeef0e427b1f1224505dbdcb3c90d7b1457fc8
NAVEEN KUMAR <nk946432@gmail.com>
2025-07-03 20:11:32 +0530

    old items

--- interview-app/backend/server.js
+++ interview-app/backend/server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require("express")
 const cors = require("cors")
-const path = require("path")
-
+const path = require("path");
+const connectDB=require('./config/db')
+const authRoutes=require("./routes/authRoutes")
 const app = express();
 
 app.use(
@@ -13,6 +14,7 @@ app.use(
 
     })
 )
+connectDB()
 
 //Midlewares
 
@@ -20,8 +22,17 @@ app.use(express.json())
 
 //Routes
 
+app.use("/api/auth",authRoutes);
+/* app.post('/api/auth/register', (req, res) => {
+    console.log(req.body); // to see incoming data in console
+    res.json({ message: 'User registered successfully' });
+  }); */
+/* app.use("api/sessions",sessionRoutes);
+app.use("api/questions",questionRoutes);
+app.use("api/ai/generate-questions",protect,generateInterviewQuestions);
+app.use("api/ai/generate-explanation",protect,generateConceptExplanation);
 //Serve uploads folder
-app.use("/uploads",express.static(path.join(__dirname,"uploads"),{}));
+app.use("/uploads",express.static(path.join(__dirname,"uploads"),{})); */
 
 //start Server
 
